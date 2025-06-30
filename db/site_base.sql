e5e484cadc9655b197e0717d3cc929975662e557
Grigoriy <foxtail25@gmail.com>
2025-06-30 09:51:08 +0300

    step 0.5

---
+++ db/site_base.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.127.126.26
+-- Время создания: Июн 30 2025 г., 09:36
+-- Версия сервера: 8.0.35
+-- Версия PHP: 8.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `site_base`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int NOT NULL,
+  `slug` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` text COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `pages`
+--
+
+INSERT INTO `pages` (`id`, `slug`, `title`, `content`) VALUES
+(1, '', 'page 1', '<div>page_1</div>'),
+(2, '', 'page 2', '<div>page_2</div>'),
+(3, '', 'page 3', '<div>page_3</div>');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- public/index.php
+++ public/index.php
@@ -1,14 +1,20 @@
 <?php
-// echo 'Hello word! (from PHP file engine)<br/>';
+echo 'Hello word! (from PHP file engine)<br/>';
 $url = $_SERVER['REQUEST_URI']; // поучаем адрес запроса
-// echo '$_SERVER["REQUEST_URI"] =' . $url;
+echo '$_SERVER["REQUEST_URI"] =' . $url;
 include('../db/connect.php');
-$query = "SELECT * FROM pages WHERE id = 1";
-$db_answer = mysqli_query($site_base_link, $query);
-$res = mysqli_fetch_assoc($db_answer);
-var_dump($res);
+
+preg_match('#/page/(\d+)#',$url, $match);
+// preg_match('#/page/(\d+)#', $url, $match);
+$id = $match[1];
+$query = "SELECT * FROM pages WHERE id = '$id'";
+$db_answer = mysqli_query($site_base_link, $query) or die(mysqli_error($site_base_link));
+$page = mysqli_fetch_assoc($db_answer);
 
 $layout = file_get_contents('layout.php');
+$layout = str_replace('{{ title in layout }}', $page['title'], $layout);
+$layout = str_replace('{{ content in layout }}', $page['content'], $layout);
+
 
 echo $layout; // Возвращаем страницу пользователю
 ?>
